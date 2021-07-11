4b5d31ee18f49c2ffadb2fb22dd3998c2f1f6051
Blackdr3464 <63839741+Blackdr3464@users.noreply.github.com>
2021-07-11 22:39:49 +0700

    add new

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 var listStudentApi = 'http://localhost:3000/listStudent';
 
-//getAPI
+//get API
 function getListStudent(callback) {
     fetch(listStudentApi)
         .then(function(reponsive) {
             return reponsive.json()
         })
         .then(callback)
-}
+};
 //start
 function start() {
-    getListStudent(renderListStudent)
+    getListStudent(function(listStudent) {
+        renderListStudent(listStudent)
+    })
 }
-start();
-//render ra trình duyệt
-function renderListStudent(liststudent) {
-    var getElementdiv = document.querySelector('#list-student');
-    var htmls = liststudent.map(function(student) {
+start()
+    //render
+function renderListStudent(listStudent) {
+    var getElenmentDiv = document.querySelector('#list-student')
+    var htmls = listStudent.map(function(student) {
         return `
         <li>
             <h4>${student.name}</h4>
             <p>Tuổi: ${student.age}</p>
             <p>Số điện thoại: ${student.phone}</p>
             <p>Email: ${student.email}</p>
-        </li>
-        `;
+        </li>`
     })
-    getElementdiv.innerHTML = htmls.join(' ')
+    getElenmentDiv.innerHTML = htmls.join(' ')
 }
